ddf39d6d06feaf0514ad478f0b8f1e15007135bf
Dominik Biškupović <86226204+dominikbiskupovic@users.noreply.github.com>
2025-06-24 17:10:11 +0200

    add migrations

---
+++ Project/Web API/Migrations/20250622153018_AddRegistrationNumber.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRegistrationNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RegistrationNumber",
+                table: "Patient",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RegistrationNumber",
+                table: "Patient");
+        }
+    }
+}
